cb73a1fed8910935d50a055a2c0889789b0e2e15
baikman <baikman@cedarville.edu>
2025-03-30 17:55:18 -0400

    Wire updates

--- HW8/Wire.cpp
+++ HW8/Wire.cpp
@@ -1,2 +1,34 @@
 #include "Gate.h"
 #include "Wire.h"
+
+void Wire::SetValue(char val) {
+	value = val;
+}
+
+void Wire::SetHistory(string hist) {
+	history = hist;
+}
+
+void Wire::SetDrives(vector<Gate*> drvs) {
+	drives = drvs;
+}
+
+char Wire::GetValue() const {
+	return value;
+}
+
+int Wire::GetIndex() const {
+	return index;
+}
+
+string Wire::GetName() const {
+	return name;
+}
+
+string Wire::GetHistory() const {
+	return history;
+}
+
+vector<Gate*> Wire::GetDrives() const {
+	return drives;
+}
--- HW8/Wire.h
+++ HW8/Wire.h
@@ -1,1 +1,28 @@
 #pragma once
+#include "Gate.h"
+#include "cctype"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Gate;
+
+class Wire {
+public:
+	Wire() : value('X'), index(0), name("wire"), history(""), drives(NULL) {};
+	void SetValue(char val);
+	void SetHistory(string hist);
+	void SetDrives(vector<Gate*> drvs);
+	char GetValue() const;
+	int GetIndex() const;
+	string GetName() const;
+	string GetHistory() const;
+	vector<Gate*> GetDrives() const;
+private:
+	char value;
+	int index;
+	string name;
+	string history;
+	vector<Gate*> drives;
+};
