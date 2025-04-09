9852edd9ba2eb453895a5bac7fdac2401cf67d25
awurster232 <awurster232@cedarville.edu>
2025-04-09 09:54:36 -0400

    04/09

---
+++ HW8/Event.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "cctype"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Queue;
+
+class Event {
+public:
+    Event() : wireName(""), time(-1), state(""), count(0);
+    Event(string name, int t, char val, int cnt) : wireName(name), time(t), state(val), count(cnt);
+
+private:
+    string wireName;
+    int time;
+    char state;
+    int count;
+};
---
+++ HW8/Queue.cpp
@@ -0,0 +1,7 @@
+#include "Gate.h"
+#include "Wire.h"
+#include "Defns.h"
+#include <iostream>
+
+int* ptr1 = new Event(wireName, time, value);
+    eventPtr.push_back(ptr1);
