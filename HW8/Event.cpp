67119f7661a97770ab7f246572ab16070473a63b
awurster232 <awurster232@cedarville.edu>
2025-04-10 11:55:54 -0400

    Input file stream

--- HW8/Event.cpp
+++ HW8/Event.cpp
@@ -2,3 +2,33 @@
 #include "Wire.h"
 #include "Defns.h"
 #include <iostream>
+
+
+
+//include at the top
+#include <iostream>
+#include <fstream>
+
+
+//include in main
+
+ifstream inFS;
+string fileName;
+
+cin >> fileName;
+
+inFS.open(fileName);
+
+//If file cannot open
+if (!inFS.is_open()) {
+    cout << "Could not open " << fileName << endl;
+    return 1;
+}
+
+
+
+//close file
+inFS.close();
+
+
+
